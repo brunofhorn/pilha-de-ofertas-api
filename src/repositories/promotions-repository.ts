c9ece1f579f16a2f2c3cde00f28283b5fd20600a
brunofhorn <b.fernandeshorn@gmail.com>
2025-05-13 23:42:48 -0300

    feat: init promotions

--- src/repositories/promotions-repository.ts
+++ src/repositories/promotions-repository.ts
@@ -2,4 +2,6 @@ import { Prisma, Promotion } from "@/generated/prisma";
 
 export interface PromotionsRepository {
     create(data: Prisma.PromotionCreateInput): Promise<Promotion>;
+    send(id: number): Promise<Promotion>
+    searchMany(query: string, page: number): Promise<Promotion[] | null>
 }
